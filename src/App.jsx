da00566d33f90003facff83c090ab7db5f0ffbe4
mkumar0802 <mkumar0802@gmail.com>
2022-07-27 20:20:45 +0530

    dark mode

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Home from './components/Home'
+
+function App() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleThemeSwitch}
+        className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
+      >
+        {theme === 'dark' ?<div className="text-amber-400"><ion-icon name="sunny"></ion-icon> </div>   :  <div className=""><ion-icon name="moon"></ion-icon>
+
+</div>}
+      </button>
+      <div className="bg-white dark:bg-slate-900 min-h-screen font-inter ">
+        <div className="max-w-5xl w-11/12 mx-auto">
+          <Home />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 dark:text-indigo-500 font-semibold'> Hello World </div>
+  )
+}
+
+
+export default Home;
